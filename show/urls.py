e633c32f132cb3e1ab99a9f2fa1cfd0de765fab3
Emma <chenemma1228@gmail.com>
2026-02-12 16:00:33 +0800

    User完/頁面+css開始(2/12)

--- show/urls.py
+++ show/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
 path("", UpdateoptionList.as_view(), name = 'updateoption_list'),
 path("<int:pk>/", UpdateoptionView.as_view(), name = 'updateoption_view'),
 path("update/",WorkCreate.as_view(), name = 'work_create'),
+path('register/', views.register_view, name='register'),
 ]
--- show/views.py
+++ show/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView,CreateView,DetailView
 from .models import *
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -27,3 +29,20 @@ class UpdateoptionView(DetailView):
         ctx = super().get_context_data(**kwargs)
         ctx['work_list'] = works.objects.filter(topic_id = self.object.id)
         return ctx 
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('role',)
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('updateoption_list')
+    else:
+        form = CustomUserCreationForm()
+        
+    return render(request, 'register.html', {'form': form})
